5bc5c45f667bffb86e08083b7c07a8de5b8e4408
sivakrishna.gunji@infosys.com <sivakrishna.gunji@infosys.com>
2022-08-17 14:53:22 +0530

    new grunt file

--- edge-extractor/Gruntfile.js
+++ edge-extractor/Gruntfile.js
@@ -1,7 +1,7 @@
 
 var apigee = require('./config.js');
 module.exports = function(grunt) {
-  var date = new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDate();
+  //var date = new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDate()+ '(' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds() + ')';
  //require('time-grunt')(grunt);
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -12,90 +12,90 @@ module.exports = function(grunt) {
         }}               // target
         },
     exportDevs: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/devs'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/devs'
     },
     exportProducts: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/products'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/products'
     },
     exportApps: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/apps'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/apps'
     },
     exportProxies: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/proxies'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/proxies'
     },
     exportTargetServers: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/targetservers'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/targetservers'
     },
     exportOrgKVM: {
-       dest: './edge-extracted-data_'+ date +'/kvm/org'
+       dest: './edge-extracted-data/kvm/org'
     },
     exportEnvKVM: {
-       dest: './edge-extracted-data_'+ date +'/kvm/env'
+       dest: './edge-extracted-data/kvm/env'
     },
     exportProxyKVM: {
-       dest: './edge-extracted-data_'+ date +'/kvm/proxy'
+       dest: './edge-extracted-data/kvm/proxy'
     },
     exportSharedFlows: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/sharedflows'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/sharedflows'
     },
     exportFlowHooks: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/flowhooks/flow_hook_config'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/flowhooks/flow_hook_config'
     },
     exportAllSpecs: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/specs'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/specs'
     },
     exportReports: {
-       dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/reports'
+       dest: './edge-extracted-data/org/'+apigee.from.org+'/reports'
     },
     exportVirtualHosts: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/virtualhosts'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/virtualhosts'
     },
     exportKeyStores: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/keystores'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/keystores'
     },
     exportReferences: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/references'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/references'
     },
     exportCaches: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/caches'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/caches'
     },
     exportRoles: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/userroles'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/userroles'
     },
     exportCollections: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/collections'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/collections'
     },
     exportAlerts: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/alerts'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/alerts'
     },
     // exportCompanies: {
-    //   dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/companies'
+    //   dest: './edge-extracted-data/org/'+apigee.from.org+'/companies'
     // },
     exportExtensions: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/extensions'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/extensions'
     },
     exportAnalyticQueries: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/queries'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/queries'
     },
     exportAnalyticExports: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/queries'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/environment/'+apigee.from.env+'/queries'
     },
     exportAuditLogs: {
-      dest: './edge-extracted-data_'+ date +'/org/'+apigee.from.org+'/audits'
+      dest: './edge-extracted-data/org/'+apigee.from.org+'/audits'
     },
     exportOrgResources: {
-      dest: './edge-extracted-data_'+ date +'/resources/org'
+      dest: './edge-extracted-data/resources/org'
     },
     exportEnvResources: {
-      dest: './edge-extracted-data_'+ date +'/resources/env'
+      dest: './edge-extracted-data/resources/env'
     },
     readCSVDevs: {
         in_devs: './input/devs.csv',
-        out_devs: './edge-extracted-data_'+ date +'/devs/'image.png
+        out_devs: './edge-extracted-data/devs/'image.png
       },
     readCSVApps: {
         in_apps: './input/apps.csv',
-        out_apps: './edge-extracted-data_'+ date +'/apps/'
+        out_apps: './edge-extracted-data/apps/'
     }
 
   });
